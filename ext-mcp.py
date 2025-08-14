8b06cab1eb650d3df77072068ca4b67dedf3ba45
Karamveer Singh <test@test.com>
2025-08-14 22:27:01 +0530

    Initial commit.

---
+++ ext-mcp.py
@@ -0,0 +1,41 @@
+# pip install fastmcp
+
+import os
+#import "/Users/admin/miniconda3/lib/python3.12/site-packages/openai/__init__.py" as openai
+import openai
+
+from mcp.server.fastmcp import FastMCP
+import math
+
+from dotenv import load_dotenv
+load_dotenv()
+
+groq_api_key = os.getenv('GROQ_API_KEY')
+groq_model = "openai/gpt-oss-120b"
+
+mcp = FastMCP("External API on LM Studio", "0.1.0")
+
+@mcp.tool()
+async def Groq(prompt: str) -> str:
+    """
+    Queries Groq API to answer the user query.
+    
+    Args:
+        prompt: The query to send to the Groq API.
+   
+    Returns:
+        The response from the Groq API as a string.
+    """
+
+    client = openai.OpenAI(
+        base_url="https://api.groq.com/openai/v1",
+        api_key=groq_api_key
+    )
+    response = client.responses.create(
+        model=groq_model,
+        input=prompt,
+    )
+    return response.output_text
+
+
+mcp.run(transport='stdio')  # Standard input/output transport
---
+++ mathmcp.py
@@ -0,0 +1,26 @@
+# pip install fastmcp
+
+
+from mcp.server.fastmcp import FastMCP
+import math
+
+mcp = FastMCP("Kams First MCP Server", "0.1.0")
+
+@mcp.tool()
+async def factorial(param1: int) -> str:
+    """
+    Returns the factorial of a number.
+    
+    Args:
+        param1: The number to calculate the factorial of.
+   
+    Returns:
+        The factorial of the input number as a string.
+    """
+    if param1 < 0:
+        return "Error: Factorial is not defined for negative numbers."
+    result = math.factorial(param1)
+    return str(result)
+
+
+mcp.run(transport='stdio')  # Standard input/output transport
